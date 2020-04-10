503626d71de5e2cebac41c83e2dddaeb9f29e223
Paul-Colucci <paulcolucci@gmail.com>
2020-04-10 02:33:20 -0400

    Add files via upload

---
+++ _aknf_ATARI_2600.bat
@@ -0,0 +1,151 @@
+md _aknf
+copy "Adventure (USA).zip" _aknf
+copy "Air Raiders (USA).zip" _aknf
+copy "Air-Sea Battle ~ Target Fun (Japan, USA).zip" _aknf
+copy "Alien (USA).zip" _aknf
+copy "Amidar (USA).zip" _aknf
+copy "Asteroids (Japan, USA).zip" _aknf
+copy "Astroblast (USA) (v1.1).zip" _aknf
+copy "Atlantis (USA).zip" _aknf
+copy "Battlezone (Japan, USA).zip" _aknf
+copy "Berzerk (Japan, USA).zip" _aknf
+copy "BMX Air Master (USA).zip" _aknf
+copy "Boing! (USA).zip" _aknf
+copy "Boxing (USA).zip" _aknf
+copy "Breakout ~ Breakaway IV (Japan, USA).zip" _aknf
+copy "Bump 'n' Jump (USA).zip" _aknf
+copy "California Games (USA).zip" _aknf
+copy "Casino ~ Poker Plus (USA).zip" _aknf
+copy "Centipede (Japan, USA).zip" _aknf
+copy "Chopper Command (USA).zip" _aknf
+copy "Circus Atari (Japan, USA).zip" _aknf
+copy "Combat ~ Tank-Plus (USA).zip" _aknf
+copy "Cosmic Ark (USA).zip" _aknf
+copy "Crazy Climber (USA).zip" _aknf
+copy "Crystal Castles (USA).zip" _aknf
+copy "Defender (USA).zip" _aknf
+copy "Demon Attack (USA) (Rev 1).zip" _aknf
+copy "Dig Dug (USA).zip" _aknf
+copy "Dodge 'Em ~ Dodger Cars (USA).zip" _aknf
+copy "Donkey Kong (USA).zip" _aknf
+copy "Donkey Kong Junior (USA).zip" _aknf
+copy "Double Dragon (USA).zip" _aknf
+copy "Dragonfire (USA).zip" _aknf
+copy "E.T. - The Extra-Terrestrial (USA).zip" _aknf
+copy "Enduro (USA).zip" _aknf
+copy "Entombed (USA).zip" _aknf
+copy "Fatal Run (USA).zip" _aknf
+copy "Fireball (USA).zip" _aknf
+copy "Fishing Derby (USA).zip" _aknf
+copy "Football (USA).zip" _aknf
+copy "Frankenstein's Monster (USA).zip" _aknf
+copy "Freeway (USA).zip" _aknf
+copy "Frogger (USA).zip" _aknf
+copy "Frogs and Flies (USA).zip" _aknf
+copy "Front Line (USA).zip" _aknf
+copy "Frostbite (USA).zip" _aknf
+copy "Galaxian (USA).zip" _aknf
+copy "Gauntlet (USA).zip" _aknf
+copy "Golf (USA).zip" _aknf
+copy "Grand Prix (USA).zip" _aknf
+copy "Gravitar (USA).zip" _aknf
+copy "Gyruss (USA).zip" _aknf
+copy "H.E.R.O. (USA).zip" _aknf
+copy "Haunted House (USA).zip" _aknf
+copy "Ice Hockey (USA).zip" _aknf
+copy "Ikari Warriors (USA).zip" _aknf
+copy "Indy 500 ~ Race (USA).zip" _aknf
+copy "Jawbreaker (USA).zip" _aknf
+copy "Joust (USA).zip" _aknf
+copy "Jr. Pac-Man (USA).zip" _aknf
+copy "Jungle Hunt (USA).zip" _aknf
+copy "Kaboom! (USA).zip" _aknf
+copy "Kangaroo (USA).zip" _aknf
+copy "Keystone Kapers (USA).zip" _aknf
+copy "Kool-Aid Man (USA).zip" _aknf
+copy "Kung-Fu Master (USA).zip" _aknf
+copy "Laser Blast (USA).zip" _aknf
+copy "Mario Bros. (USA).zip" _aknf
+copy "Maze Craze - A Game of Cops 'n Robbers ~ Maze Mania - A Game of Cops 'n Robbers (USA).zip" _aknf
+copy "MegaMania - A Space Nightmare (USA).zip" _aknf
+copy "Millipede (USA).zip" _aknf
+copy "Missile Command (USA).zip" _aknf
+copy "Montezuma's Revenge - Featuring Panama Joe (USA).zip" _aknf
+copy "Moon Patrol (USA).zip" _aknf
+copy "Mountain King (USA).zip" _aknf
+copy "Mouse Trap (USA).zip" _aknf
+copy "Mr. Do! (USA).zip" _aknf
+copy "Mr. Do!'s Castle (USA).zip" _aknf
+copy "Ms. Pac-Man (USA).zip" _aknf
+copy "Night Driver (USA).zip" _aknf
+copy "Othello (USA).zip" _aknf
+copy "Outlaw ~ Gunslinger (USA).zip" _aknf
+copy "Pac-Man (USA).zip" _aknf
+copy "Party Mix - Bop a Buggy, Tug of War, Wizard's Keep, Down on the Line, Handcar (USA).zip" _aknf
+copy "Pengo (USA).zip" _aknf
+copy "Pete Rose Baseball (USA).zip" _aknf
+copy "Phaser Patrol (USA).zip" _aknf
+copy "Phoenix (USA).zip" _aknf
+copy "Pitfall II - Lost Caverns (USA).zip" _aknf
+copy "Pitfall! - Pitfall Harry's Jungle Adventure (USA).zip" _aknf
+copy "Polaris (USA).zip" _aknf
+copy "Pole Position (USA).zip" _aknf
+copy "Popeye (USA).zip" _aknf
+copy "Pressure Cooker (USA).zip" _aknf
+copy "Private Eye (USA).zip" _aknf
+copy "Q-bert (USA).zip" _aknf
+copy "Radar Lock (USA).zip" _aknf
+copy "Raiders of the Lost Ark (USA).zip" _aknf
+copy "Revenge of the Beefsteak Tomatoes (USA).zip" _aknf
+copy "Riddle of the Sphinx (USA).zip" _aknf
+copy "River Raid (USA).zip" _aknf
+copy "River Raid II (USA).zip" _aknf
+copy "Road Runner (USA).zip" _aknf
+copy "Robot Tank (USA).zip" _aknf
+copy "Seaquest (USA).zip" _aknf
+copy "Secret Quest (USA).zip" _aknf
+copy "Sir Lancelot (USA).zip" _aknf
+copy "Skiing (USA).zip" _aknf
+copy "Sky Diver ~ Dare Diver (USA).zip" _aknf
+copy "Smurf - Rescue in Gargamel's Castle (USA).zip" _aknf
+copy "Solar Fox (USA).zip" _aknf
+copy "Solar Storm (USA).zip" _aknf
+copy "Solaris (USA).zip" _aknf
+copy "Space Invaders (USA).zip" _aknf
+copy "Spider Fighter (USA).zip" _aknf
+copy "Spider-Man (USA).zip" _aknf
+copy "Sprint Master (USA).zip" _aknf
+copy "Spy Hunter (USA).zip" _aknf
+copy "Stampede (USA).zip" _aknf
+copy "Star Raiders (USA).zip" _aknf
+copy "Star Trek - Strategic Operations Simulator (USA).zip" _aknf
+copy "Star Voyager (USA).zip" _aknf
+copy "Star Wars - Jedi Arena (USA).zip" _aknf
+copy "Star Wars - Return of the Jedi - Death Star Battle (USA).zip" _aknf
+copy "Star Wars - The Arcade Game (USA).zip" _aknf
+copy "Star Wars - The Empire Strikes Back (USA).zip" _aknf
+copy "Stargate (USA).zip" _aknf
+copy "StarMaster (USA).zip" _aknf
+copy "Submarine Commander (USA).zip" _aknf
+copy "Super Breakout (USA).zip" _aknf
+copy "Super Challenge Football (USA).zip" _aknf
+copy "Super Football (USA).zip" _aknf
+copy "Superman (USA).zip" _aknf
+copy "Surround ~ Chase (USA).zip" _aknf
+copy "Tac-Scan (USA).zip" _aknf
+copy "Tapper (USA).zip" _aknf
+copy "Threshold (USA).zip" _aknf
+copy "Thunderground (USA).zip" _aknf
+copy "Tomcat - The F-14 Fighter Simulator (USA).zip" _aknf
+copy "Track and Field (USA).zip" _aknf
+copy "Trick Shot (USA).zip" _aknf
+copy "Turmoil (USA).zip" _aknf
+copy "Up 'n Down (USA).zip" _aknf
+copy "Video Pinball ~ Arcade Pinball (USA).zip" _aknf
+copy "Warlords (USA).zip" _aknf
+copy "Winter Games (USA).zip" _aknf
+copy "Wizard of Wor (USA).zip" _aknf
+copy "Word Zapper (USA).zip" _aknf
+copy "Worm War I (USA).zip" _aknf
+copy "Yars' Revenge (USA).zip" _aknf
+copy "Zaxxon (USA).zip" _aknf
